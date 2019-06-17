f1802280ff228e6287f9ddbe600b825ecd13faf9
Blackhippy <blackhippy@protonmail.com>
2019-06-17 12:27:44 -0500

    Minor Update

---
+++ scrips/.bin/2bws.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+# display workspace status
+# requires wmctrl
+
+# get the current workspace
+ws=$( xprop -root _NET_CURRENT_DESKTOP | sed -e 's/_NET_CURRENT_DESKTOP(CARDINAL) = //' )
+
+# icons
+
+CURRENT=∙
+OCCUPIED=∙
+UNOCCUPIED=⋅
+
+# colors
+fg="#faf6f5"
+fg1="#7d7579"
+
+#  print workspaces to stdout
+draw() {
+    for i in {0..5}; do
+        # get the number of windows in each workspace
+        windows=$( wmctrl -l | cut -d ' ' -f3 | grep $i | wc -l )
+
+        if [[ $i -eq $ws ]]
+        then
+            # current workspace
+            echo -ne "%{F$fg}  "
+        else
+            echo -ne "%{F$fg1}  "
+        fi
+    done
+}
+
+draw
